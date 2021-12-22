e2c4872a59ff85c2643989b10861704950cd93f0
amrayman100 <amrayman1996@yahoo.com>
2021-12-22 13:46:52 +0200

    finishing touches in cypress

--- cypress-e2e/cypress/integration/phone-app/phone-book-view.spec.js
+++ cypress-e2e/cypress/integration/phone-app/phone-book-view.spec.js
@@ -1,60 +1,58 @@
-import { mockGetNumberList, mockGetNumberListPageTwo } from "../../utils/mocking";
+import {
+  mockGetNumberList
+} from "../../utils/mocking";
 
 beforeEach(() => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      return false
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
+
+  cy.server();
+  mockGetNumberList();
+  Cypress.on("window:before:load", (win) => {
+    Object.defineProperty(win, "self", {
+      get: () => {
+        return window.top;
+      },
     });
-
-    cy.server();
-    mockGetNumberList();
-    Cypress.on('window:before:load', (win) => {
-      Object.defineProperty(win, 'self', {
-        get: () => {
-          return window.top
-        }
-      })
-    })
-    cy.visit('/')
+  });
+  cy.visit("/");
 });
 
-
-it('Should check that the table renders 10 elements', () => {
+it("Should check that the table renders 10 elements", () => {
   mockGetNumberList();
-  cy.wait('@getAllNumbers');
+  cy.wait("@getAllNumbers");
   cy.get("#phoneTable")
-  .find("tr")
-  .then((row) => {
-    expect(row.length-1).to.equal(10)
-    cy.log(row.length);
-  });
+    .find("tr")
+    .then((row) => {
+      expect(row.length - 1).to.equal(10);
+      cy.log(row.length);
+    });
 });
 
-
-it('Should check that the table when the user clicks next it renders 10 elements', () => {
-  cy.get('#next').click();
-  cy.wait('@getAllNumbersPageTwo');
+it("Should check that the table when the user clicks next it renders 10 elements", () => {
+  cy.get("#next").click();
+  cy.wait("@getAllNumbersPageTwo");
   cy.get("#phoneTable")
-  .find("tr")
-  .then((row) => {
-    expect(row.length-1).to.equal(10)
-    cy.log(row.length);
-  });
+    .find("tr")
+    .then((row) => {
+      expect(row.length - 1).to.equal(10);
+      cy.log(row.length);
+    });
 });
 
-
-it('Should check that the table when the user clicks previous it renders 10 elements', () => {
+it("Should check that the table when the user clicks previous it renders 10 elements", () => {
   mockGetNumberList();
-  cy.wait('@getAllNumbers');
-  cy.get('#previous').click();
+  cy.wait("@getAllNumbers");
+  cy.get("#previous").click();
   cy.get("#phoneTable")
-  .find("tr")
-  .then((row) => {
-    expect(row.length-1).to.equal(10)
-    cy.log(row.length);
-  });
+    .find("tr")
+    .then((row) => {
+      expect(row.length - 1).to.equal(10);
+      cy.log(row.length);
+    });
 });
 
-it('Should click on dark mode button and backround changes to dark',() =>{
-  cy.get('#toggleViewMode').click();
-})
-
+it("Should click on dark mode button and backround changes to dark", () => {
+  cy.get("#toggleViewMode").click();
+});
--- cypress-e2e/cypress/utils/mocking.js
+++ cypress-e2e/cypress/utils/mocking.js
@@ -1,43 +1,33 @@
-import { paginatedListOfTenNumbers, paginatedListOfTenNumbersPageTwo } from "./testUtils"
+import {
+  paginatedListOfTenNumbers,
+  paginatedListOfTenNumbersPageTwo,
+} from "./testUtils";
 
-export function mockGetNumberList(){
-
+export function mockGetNumberList() {
   cy.server();
-    cy.route({
-        method: 'GET',
-        url: '**/api/v1/numbers?page=0&size=10',
-        status: 200,
-        response:
-            paginatedListOfTenNumbers()
-        ,
-        delay: 50,
-        headers: {
-          'X-Token': null
-        },
-        onRequest: (xhr) => {
-    
-        },
-        onResponse: (xhr) => {
-    
-        }
-      }).as('getAllNumbers')
+  cy.route({
+    method: "GET",
+    url: "**/api/v1/numbers?page=0&size=10",
+    status: 200,
+    response: paginatedListOfTenNumbers(),
+    delay: 50,
+    headers: {
+      "X-Token": null,
+    },
+    onRequest: (xhr) => {},
+    onResponse: (xhr) => {},
+  }).as("getAllNumbers");
 
-      cy.route({
-        method: 'GET',
-        url: '**/api/v1/numbers?page=1&size=10',
-        status: 200,
-        response:
-        paginatedListOfTenNumbersPageTwo()
-        ,
-        delay: 50,
-        headers: {
-          'X-Token': null
-        },
-        onRequest: (xhr) => {
-    
-        },
-        onResponse: (xhr) => {
-    
-        }
-      }).as('getAllNumbersPageTwo')
+  cy.route({
+    method: "GET",
+    url: "**/api/v1/numbers?page=1&size=10",
+    status: 200,
+    response: paginatedListOfTenNumbersPageTwo(),
+    delay: 50,
+    headers: {
+      "X-Token": null,
+    },
+    onRequest: (xhr) => {},
+    onResponse: (xhr) => {},
+  }).as("getAllNumbersPageTwo");
 }
